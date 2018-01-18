2da28315277893b494813bb2db8ac44e1d9d1989
Michael Howard <mhowardx77@gmail.com>
2018-01-17 21:57:02 -0500

    added countdown timer to march 3

--- app.js
+++ app.js
@@ -8,4 +8,20 @@ setInterval(function() {
     .end()
     .appendTo('#slideshow');
 },  4000);
-console.log("app.js has been loaded...");
+
+function getTimeRemaining(endtime){
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var days = Math.floor( t/(1000*60*60*24) );
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+var time = getTimeRemaining('March 3 2018').days;
+document.getElementById("daysrem").innerHTML = time;
