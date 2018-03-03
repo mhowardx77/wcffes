8b9222d3103bbdc5f71fb0622893fbb5655435fe
Michael Howard <mhowardx77@gmail.com>
2018-03-02 23:38:58 -0500

    content update

--- app.js
+++ app.js
@@ -8,20 +8,3 @@ setInterval(function() {
     .end()
     .appendTo('#slideshow');
 },  4000);
-
-function getTimeRemaining(endtime){
-  var t = Date.parse(endtime) - Date.parse(new Date());
-  var seconds = Math.floor( (t/1000) % 60 );
-  var minutes = Math.floor( (t/1000/60) % 60 );
-  var hours = Math.floor( (t/(1000*60*60)) % 24 );
-  var days = Math.floor( t/(1000*60*60*24) );
-  return {
-    'total': t,
-    'days': days,
-    'hours': hours,
-    'minutes': minutes,
-    'seconds': seconds
-  };
-}
-var time = getTimeRemaining('March 3 2018').days;
-document.getElementById("daysrem").innerHTML = time;
